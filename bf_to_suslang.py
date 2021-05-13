710d6c36beac3d64c21485f9dfcdffc80b2ca7bb
Dynamism <36286620+Dhhoyt@users.noreply.github.com>
2021-05-13 23:56:06 +0000

    converter

---
+++ bf_to_suslang.py
@@ -0,0 +1,20 @@
+input_string="++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
+output = ""
+for char in input_string:
+    if char == "+":
+        output += "sus "
+    elif char == "-":
+        output += "sussy "
+    elif char == "[":
+        output += "among "
+    elif char == "]":
+        output += "us "
+    elif char == ">":
+        output += "crewmate "
+    elif char == "<":
+        output += "imposter "
+    elif char == ".":
+        output += "eject "
+    elif char == "<":
+        output += "vote "
+print(output)
